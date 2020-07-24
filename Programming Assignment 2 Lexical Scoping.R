5d92a11ce17768a9f810e1bd0fe261ebe86cd776
Harrison Bardwell <hbardwell@smartercapitalholdings.com.au.local>
2020-07-24 16:13:56 +1000

    Programming Assignment 2: Lexical Scoping

---
+++ Programming Assignment 2 Lexical Scoping.R
@@ -0,0 +1,33 @@
+#invert matrix
+
+#swap out the getmean for getinverse
+#swap out the setmean for setinverse
+#keep m as is for simplicity
+#change mean for inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+cacheinverse <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- inverse(data, ...)
+  x$setinverse(m)
+  m
+}
